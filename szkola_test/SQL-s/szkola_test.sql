b587cbf36f55e3856653e5e51d744290c748b2eb
Kowalsky Opcje <piotrekxyz@tlen.pl>
2018-10-12 17:03:11 +0200

    Update szkola_test.sql

--- szkola_test/SQL-s/szkola_test.sql
+++ szkola_test/SQL-s/szkola_test.sql
@@ -3,7 +3,7 @@ create table Instrumenty(Id tinyint primary key identity(1,1), Nazwa varchar(20)
 create table Uczniowie(Id smallint primary key identity(1,1),
 Imie varchar(20) not null, Nazwisko varchar(30) not null,
 Cykl tinyint not null constraint sprawdz_cykl check(Cykl = 4 or Cykl = 6),
-Klasa tinyint not null constraint sprawdz_klase check(len(Klasa) = 1 and isnumeric(Klasa) = 1),
+Klasa tinyint not null constraint sprawdz_klase check(len(Klasa) = 1),
 Instrument varchar(20), Nauczyciel varchar(20),
 Pesel varchar(11) not null unique constraint sprawdz_pesel_uczniowie check(len(Pesel) = 11 and isnumeric(Pesel) = 1),
 Absolwent tinyint constraint sprawdz_absolwenta check(Absolwent = 0 or Absolwent = 1),
